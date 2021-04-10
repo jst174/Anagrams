88ba37290e8c178ee2456e6b0bfcc4012d53390c
Eugene Stepanenko <jst1749.7@gmail.com>
2021-04-10 12:06:25 +0500

    add methods reverseString and reverseWord to Reverse class; add the imlementation of the main method in the Anagram class

--- src/ua/com/foxminded/anagrams/Reverse.java
+++ src/ua/com/foxminded/anagrams/Reverse.java
@@ -1,5 +1,39 @@
 package ua.com.foxminded.anagrams;
 
 public class Reverse {
+	public static String reverseString(String input) {
+		String[] inputString = input.split(" ");
+		String[] outputString = new String[inputString.length];
 
+		int i = 0;
+		for (String string : inputString) {
+
+			outputString[i] = reverseWord(string);
+			i++;
+		}
+
+		return String.join(" ", outputString);
+
+	}
+
+	private static String reverseWord(String input) {
+
+		char[] inputWord = input.toCharArray();
+
+		for (int i = 0, j = inputWord.length - 1; i < j;) {
+
+			if (!Character.isLetter(inputWord[i])) {
+				i++;
+			} else if (!Character.isLetter(inputWord[j])) {
+				j--;
+			} else {
+				char tmp = inputWord[j];
+				inputWord[j] = inputWord[i];
+				inputWord[i] = tmp;
+				i++;
+				j--;
+			}
+		}
+		return String.valueOf(inputWord);
+	}
 }
