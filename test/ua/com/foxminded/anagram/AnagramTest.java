b8e7f3abf75c11789fa11715087b17cab59b12b7
Eugene Stepanenko <jst1749.7@gmail.com>
2021-04-19 20:12:46 +0500

    change editorconfig, rename artifactId in pom.xml, transfer versions in properties

--- test/ua/com/foxminded/anagram/AnagramTest.java
+++ test/ua/com/foxminded/anagram/AnagramTest.java
@@ -8,50 +8,50 @@ import org.junit.jupiter.api.Test;
 
 class AnagramTest {
 
-	private Anagram anagram;
+    private Anagram anagram;
 
-	@BeforeEach
-	void setUp() {
-		anagram = new Anagram();
-	}
+    @BeforeEach
+    void setUp() {
+        anagram = new Anagram();
+    }
 
-	@Test
-	void reverseString_argumentIsNull_ShouldThrowException() {
-		assertThrows(IllegalArgumentException.class, () -> anagram.reverseString(null));
-	}
+    @Test
+    void reverseString_argumentIsNull_ShouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> anagram.reverseString(null));
+    }
 
-	@Test
-	void reverseString_argumentIsEmpty_ShouldReturnEmptyString() {
-		assertEquals("", anagram.reverseString(""));
-	}
+    @Test
+    void reverseString_argumentIsEmpty_ShouldReturnEmptyString() {
+        assertEquals("", anagram.reverseString(""));
+    }
 
-	@Test
-	void reverseString_WordWithLettersOnly_ShouldReverseWord() {
-		assertEquals("dcba", anagram.reverseString("abcd"));
-	}
+    @Test
+    void reverseString_WordWithLettersOnly_ShouldReverseWord() {
+        assertEquals("dcba", anagram.reverseString("abcd"));
+    }
 
-	@Test
-	void reverseString_WordWithSomeNonLettersSymbol_NonLettersSymbolsShouldStayOnTheSamePlaces() {
-		assertEquals("d1cba!", anagram.reverseString("a1bcd!"));
-	}
+    @Test
+    void reverseString_WordWithSomeNonLettersSymbol_NonLettersSymbolsShouldStayOnTheSamePlaces() {
+        assertEquals("d1cba!", anagram.reverseString("a1bcd!"));
+    }
 
-	@Test
-	void reverseString_WordWithNonLettersSymbolsOnly_ShouldNotReverse() {
-		assertEquals("12@3!", anagram.reverseString("12@3!"));
-	}
+    @Test
+    void reverseString_WordWithNonLettersSymbolsOnly_ShouldNotReverse() {
+        assertEquals("12@3!", anagram.reverseString("12@3!"));
+    }
 
-	@Test
-	void reverseString_TextWithWordsConsistingOnlyOfLetters_ShouldReverseAllWords() {
-		assertEquals("dcba hgfe", anagram.reverseString("abcd efgh"));
-	}
+    @Test
+    void reverseString_TextWithWordsConsistingOnlyOfLetters_ShouldReverseAllWords() {
+        assertEquals("dcba hgfe", anagram.reverseString("abcd efgh"));
+    }
 
-	@Test
-	void reverseString_TextWithWordsConsistingOfSomeNonLettersSymbol_ShouldReverseLettersOnly() {
-		assertEquals("d1cba hgf!e", anagram.reverseString("a1bcd efg!h"));
-	}
+    @Test
+    void reverseString_TextWithWordsConsistingOfSomeNonLettersSymbol_ShouldReverseLettersOnly() {
+        assertEquals("d1cba hgf!e", anagram.reverseString("a1bcd efg!h"));
+    }
 
-	@Test
-	void testReverseString_TextWithWordsConsistingOfOnlyNonLettersSymbol_ShouldReturnSameText() {
-		assertEquals("12%3! 45@6^", anagram.reverseString("12%3! 45@6^"));
-	}
+    @Test
+    void testReverseString_TextWithWordsConsistingOfOnlyNonLettersSymbol_ShouldReturnSameText() {
+        assertEquals("12%3! 45@6^", anagram.reverseString("12%3! 45@6^"));
+    }
 }
